5114f2a5f3ffb032a96f12951496310a49db86b9
gperson <GrantPerson@Grant.local>
2014-12-05 14:07:32 -0600

    Update to use Al Kharid mine

--- MiningBot.sikuli/MiningBot.py
+++ MiningBot.sikuli/MiningBot.py
@@ -1,28 +1,48 @@
 #Outer determines the number of inventories to fill and drop (Also prevent infinite loop)
-outer = 3
+pickUpRune = True #flag for if you want to pick up rune, faster if false
+outer = 4
 while outer > 0 :
-    inner = 1
-    while inner < 13 :      
-        if(exists(Pattern("ore-1.png").similar(0.56))) :        
-            click(Pattern("ore-1.png").similar(0.56))
-            sleep(2.25)
-            try:
-                click(Pattern("ore-2.png").similar(0.44))
-            except:
-                sleep(.5)
-            sleep(7)
-        inner = inner+1
+    notFull = True
+    stop = 0
+    while (notFull and stop < 40):      
+        foundRune = False
+        if(exists("iron-ore-1.png")) :        
+            if foundRune :
+                sleep(3.5) #Time to run back from getting the rune
+                foundRune = False
+            click(Pattern("iron-ore-1.png").similar(0.75))
+            sleep(1.75) #Time it takes to get the ore
+        if(exists(Pattern("iron-ore-2.png").similar(0.75))) :
+            click(Pattern("iron-ore-2.png").similar(0.75))
+            sleep(1.75) #Time it takes to get the ore
 
-    #Loop to drop all ores (stop used to prevent infinite loop)
+        if(stop % 6 == 0 and pickUpRune):
+            if(exists(Pattern("fire-rune.png").similar(0.60))) : #Check ever other cycle, increase productivity
+                click(Pattern("fire-rune.png").similar(0.60))
+                foundRune = True
+                sleep(1.5) #Time it takes to run and get the rune
+        else :
+            sleep(1)
+        if(stop > 11): #Check if full only after 11 cycles, increase productivity
+           
+            if(False): #TODO Check for gems
+                if(exists("sapphire.png")):
+                    sleep(.05)#TODO drop
+                if(exists("emerald.png")):
+                    sleep(.05)#TODO drop
+               
+            if(exists(Pattern("full-inventory.png").similar(0.75))):
+                notFull = False
+        stop = stop + 1
+    #Loop to drop all ores 
     stop = 0    
-    while (exists(Pattern("action-ore.png").similar(0.95)) and stop < 40): 
+    while (stop < 26): 
         try:
-            rightClick(Pattern("action-ore.png").similar(0.95))   
-            sleep(.25)    
+            rightClick(Pattern("action-ore.png").similar(0.65))   
+            sleep(.1)    
             click(Pattern("action-ore-menu.png").similar(0.39).targetOffset(-25,-7))
-            sleep(.25)
+            sleep(.1)
         except:
-            sleep(.5)
+            sleep(.25)
         stop = stop + 1;
-        #TODO Add logic to drop gems
     outer = outer - 1
